b487cddd11a175e888a0dbf8d08aaffbdfeb7522
Eduardo Santos <63939209+Eduardomlcs@users.noreply.github.com>
2024-06-19 00:35:07 +0100

    testing

--- scripts/tribetroop.js
+++ scripts/tribetroop.js
@@ -8,7 +8,7 @@ function getCurrentServer() {
     
 }
 function getTroopLink(currentServer, playerId) {
-  return `${currentServer}/game.php?screen=ally&mode=members_defense&player_id=${playerId}`;
+  return `https://${currentServer}/game.php?screen=ally&mode=members_defense&player_id=${playerId}`;
 }
 
 //https://xs3.tribalwars.cash/game.php?village=7&screen=ally&mode=members_troops
@@ -21,7 +21,35 @@ function main() {
         let link = getTroopLink(currentServer, playerId);
         links.push(link);
     }
+    $.getAll(links, (i, data) => {
+        let troops = $(data).find("td");
+        console.log(troops);
+    });
     console.log(links);
 }   
 
 main();
+
+
+
+        $.getAll(linksODSTotal, (i, data) => {
+            if ($(data).find(".lit-item")[3] != undefined) {
+                temp = $(data).find(".lit-item")
+                x = temp[3].innerText;
+                console.log(x);
+                if (x.indexOf(" Mil.") > -1) {
+                    x = x.replace(" Mil.", "");
+                    x = x.replace(",", "");
+                    x = parseInt(x) * 10000;
+                    x=x.toString();
+                }
+                ODSTotalperPlayer.push(x.replace(/\./g, ','));
+            }
+            else {
+                ODSTotalperPlayer.push("0");
+            }
+
+        },
+            (error) => {
+                console.error(error);
+            });
