9ce9ee9480b6d2de6933d262995b912857a7f7e3
Eduardo Santos <63939209+Eduardomlcs@users.noreply.github.com>
2024-06-19 00:53:19 +0100

    Added getall()

--- scripts/tribetroop.js
+++ scripts/tribetroop.js
@@ -13,6 +13,47 @@ function getTroopLink(currentServer, playerId) {
 
 //https://xs3.tribalwars.cash/game.php?village=7&screen=ally&mode=members_troops
 
+$.getAll = function (
+    urls, // array of URLs
+    onLoad, // called when any URL is loaded, params (index, data)
+    onDone, // called when all URLs successfully loaded, no params
+    onError // called when a URL load fails or if onLoad throws an exception, params (error)
+) {
+    var numDone = 0;
+    var lastRequestTime = 0;
+    var minWaitTime = 200; // ms between requests
+    loadNext();
+    function loadNext() {
+        if (numDone == urls.length) {
+            onDone();
+            return;
+        }
+
+        let now = Date.now();
+        let timeElapsed = now - lastRequestTime;
+        if (timeElapsed < minWaitTime) {
+            let timeRemaining = minWaitTime - timeElapsed;
+            setTimeout(loadNext, timeRemaining);
+            return;
+        }
+        console.log('Getting ', urls[numDone]);
+        lastRequestTime = now;
+        $.get(urls[numDone])
+            .done((data) => {
+                try {
+                    onLoad(numDone, data);
+                    ++numDone;
+                    loadNext();
+                } catch (e) {
+                    onError(e);
+                }
+            })
+            .fail((xhr) => {
+                onError(xhr);
+            })
+    }
+};
+
 function main() {
     let currentServer = getCurrentServer();
     let links = [];
