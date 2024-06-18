e978f931b2024c68546f668a8d4b99839c8be957
Eduardo Santos <63939209+Eduardomlcs@users.noreply.github.com>
2024-06-19 00:20:05 +0100

    Collects links for ajax calls

--- scripts/tribetroop.js
+++ scripts/tribetroop.js
@@ -1,5 +1,27 @@
-const tribeMembersId = [7669464];
+const playerIds = [7669464];
 
+function getCurrentServer() {
+    const url = window.location.href;
+    const re = new RegExp("(?<=//).*(?=[\/])");
+    let server = re.exec(url);
+    return server;
+    
+}
+function getTroopLink(currentServer, playerId) {
+  return `${currentServer}/game.php?screen=ally&mode=members_defense&player_id=${playerId}`;
+}
 
 //https://xs3.tribalwars.cash/game.php?village=7&screen=ally&mode=members_troops
 
+function main() {
+    let currentServer = getCurrentServer();
+    let links = [];
+    console.log("Current server: ", currentServer);
+    for (let playerId of playerIds) {
+        let link = getTroopLink(currentServer, playerId);
+        links.push(link);
+    }
+    console.log(links);
+}   
+
+main();
